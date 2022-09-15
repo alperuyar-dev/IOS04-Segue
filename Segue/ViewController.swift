3a5b92701e3fdcfe852e806bfedcfeb8bf667225
alperuyar-dev <alperuyar.dev@gmail.com>
2022-09-15 14:40:01 +0300

    Initial Commit

---
+++ Segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Segue
+//
+//  Created by Alper Uyar on 15.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
